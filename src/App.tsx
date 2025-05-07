082b7ea67f7059126ce97234dc97f64873c6390d
7R0N1X <techsoftec.tronix@gmail.com>
2025-05-07 10:55:02 -0500

    feat: add Tailwind Color Form and Color Palette component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { Header } from "@components/Header";
+import { ColorPalette } from "@components/ColorPalette";
+import { TailwindColorForm } from "@components/TailwindColorForm";
 export const App = () => {
   return (
-    <>
+    <div className="mx-auto max-w-screen-2xl space-y-[60px] pt-[96px]">
       <Header />
-    </>
+      <TailwindColorForm />
+      <ColorPalette />
+    </div>
   );
 };
