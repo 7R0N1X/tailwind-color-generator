99eba773354aec6f48f4fbf41ec1173e08f38cf2
7R0N1X <techsoftec.tronix@gmail.com>
2025-05-07 10:42:04 -0500

    feat: change the line height

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
   return (
     <section className="mx-auto w-fit space-y-6 text-center text-gray-50">
-      <h1 className="text-5xl font-bold">
+      <h1 className="text-5xl leading-[58px] font-bold">
         Tailwind CSS <br /> Color Generator
       </h1>
       <p>Enter a name and hexadecimal code to create a custom color palette</p>
