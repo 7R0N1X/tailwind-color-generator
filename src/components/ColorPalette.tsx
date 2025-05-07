802bf6ae657b04195e424802aaac2553b26d3e16
7R0N1X <techsoftec.tronix@gmail.com>
2025-05-07 10:54:08 -0500

    feat: add Color Palette component

---
+++ src/components/ColorPalette.tsx
@@ -0,0 +1,19 @@
+import { ColorSwatch } from "@components/ColorSwatch";
+
+export const ColorPalette = () => {
+  return (
+    <div className="mx-auto flex w-fit gap-5 border">
+      <ColorSwatch />
+      <ColorSwatch />
+      <ColorSwatch />
+      <ColorSwatch />
+      <ColorSwatch />
+      <ColorSwatch />
+      <ColorSwatch />
+      <ColorSwatch />
+      <ColorSwatch />
+      <ColorSwatch />
+      <ColorSwatch />
+    </div>
+  );
+};
