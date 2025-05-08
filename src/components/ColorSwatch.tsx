c3a2abc159afd8cedcc7dd0b3f3f43890d41ba29
7R0N1X <techsoftec.tronix@gmail.com>
2025-05-08 12:02:08 -0500

    feat: add dynamic background according to color variant

--- src/components/ColorSwatch.tsx
+++ src/components/ColorSwatch.tsx
@@ -1,15 +1,18 @@
+import tinycolor from "tinycolor2";
+
 interface ColorSwatchProps {
-  bgColor?: string;
-  variant?: number;
+  bgColor: string | any;
+  variant: string;
 }
 
 export const ColorSwatch = ({ bgColor, variant }: ColorSwatchProps) => {
+  const hex = "#" + tinycolor(bgColor).toHex();
   return (
     <div className="w-fit space-y-2">
-      <div className="size-[117px] rounded-lg bg-amber-400"></div>
-      <div className="text-gray-50 text-xs">
-        <p className="mx-auto w-fit variant">50</p>
-        <p className="mx-auto w-fit uppercase hex-display">#f8f6f6</p>
+      <div className="size-[117px] rounded-lg border border-gray-50/10" style={{ backgroundColor: `${hex}` }}></div>
+      <div className="text-xs text-gray-50">
+        <p className="variant mx-auto w-fit">{variant}</p>
+        <p className="hex-display mx-auto w-fit uppercase">{hex}</p>
       </div>
     </div>
   );
