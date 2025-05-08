c2b2d090c37f0b74a3cf1c826a76d9eae41f0c9a
7R0N1X <techsoftec.tronix@gmail.com>
2025-05-08 16:21:49 -0500

    feat: add padding top for mobile devices

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { UiExamples } from "@components/UiExamples";
 import { BlurredCircle } from "@components/BlurredCircle";
 export const App = () => {
   return (
-    <div className="relative mx-auto max-w-[1489px] space-y-[60px] overflow-x-hidden pt-[96px]">
+    <div className="relative mx-auto max-w-[1489px] space-y-[60px] overflow-x-hidden pt-20 sm:pt-[96px]">
       <BlurredCircle />
       <Header />
       <TailwindColorForm />
