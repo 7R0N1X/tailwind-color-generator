928d8b276a32bf72fc820cdc402553821e03f7c3
7R0N1X <techsoftec.tronix@gmail.com>
2025-05-08 12:06:15 -0500

    feat: add ColorPalette component to display dynamically generated colors

--- src/components/ColorPalette.tsx
+++ src/components/ColorPalette.tsx
@@ -1,19 +1,14 @@
 import { ColorSwatch } from "@components/ColorSwatch";
+import { useColorPicker } from "@hooks/useColorPicker";
 
 export const ColorPalette = () => {
+  const { colorPalette } = useColorPicker();
+
   return (
-    <div className="mx-auto flex w-fit gap-5 border">
-      <ColorSwatch />
-      <ColorSwatch />
-      <ColorSwatch />
-      <ColorSwatch />
-      <ColorSwatch />
-      <ColorSwatch />
-      <ColorSwatch />
-      <ColorSwatch />
-      <ColorSwatch />
-      <ColorSwatch />
-      <ColorSwatch />
+    <div className="mx-auto flex w-fit gap-5">
+      {Object.entries(colorPalette).map(([key, value]) => (
+        <ColorSwatch key={key} variant={key} bgColor={value} />
+      ))}
     </div>
   );
 };
