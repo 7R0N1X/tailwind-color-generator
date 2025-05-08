cf9ce5ff42cbea99b9383937212d418ee48aed5f
7R0N1X <techsoftec.tronix@gmail.com>
2025-05-08 14:41:19 -0500

    feat: add dinamyc text color

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,17 +3,14 @@ import tinycolor from "tinycolor2";
 
 export const Card = () => {
   const { colorPalette } = useColorPicker();
-  const hex = tinycolor(colorPalette["500"]).toHex();
+  const gradientColor = tinycolor(colorPalette["800"]).toHex();
+  const textColor = tinycolor(colorPalette["50"]).toHex();
 
   return (
-    <div className="relative flex h-full w-full items-end overflow-hidden rounded-xl p-6">
-      <img
-        src="/images/VzJjPuk53sk.avif"
-        alt="Person writing code on a laptop"
-        className="absolute inset-0 z-0 h-full w-full object-cover"
-      />
-      <div className="z-10t absolute inset-0" style={{ backgroundImage: `linear-gradient(to top, #${hex}, transparent)` }}></div>
-      <h2 className="relative z-20 text-4xl leading-[44px] font-semibold text-gray-50">Modern Web Development</h2>
+    <div className="relative flex h-full w-full items-end overflow-hidden rounded-xl border border-gray-50/10 p-6">
+      <img src="/images/VzJjPuk53sk.avif" alt="Person writing code on a laptop" className="absolute inset-0 z-0 h-full w-full object-cover" />
+      <div className="z-10t absolute inset-0" style={{ backgroundImage: `linear-gradient(to top, #${gradientColor}, transparent)` }}></div>
+      <h2 className="relative z-20 text-4xl leading-[44px] font-semibold" style={{ color: `#${textColor}` }}>Modern Web Development</h2>
     </div>
   );
 };
