a2e609079858c0c03c350906c3d2cf3e941ae4b9
7R0N1X <techsoftec.tronix@gmail.com>
2025-05-08 11:52:20 -0500

    feat: add file to generate color palette

---
+++ src/utils/generatePalette.ts
@@ -0,0 +1,34 @@
+export const generateColorPalette = (colorHsl: string) => {
+  const cleanHsl = colorHsl.replace("hsl(", "").replace(")", "");
+  const [h, s, l] = cleanHsl.split(",");
+
+  const hslValue = {
+    h: Number(h),
+    s: Number(s.replace("%", "")),
+    l: Number(l.replace("%", "")),
+  };
+
+  const levels: Record<number, number> = {
+    50: 97,
+    100: 90,
+    200: 80,
+    300: 70,
+    400: 60,
+    500: hslValue.l,
+    600: 40,
+    700: 30,
+    800: 20,
+    900: 12,
+    950: 6,
+  };
+
+  const shades: Record<number, string> = {};
+
+  for (const [level, l] of Object.entries(levels)) {
+    const key = Number(level);
+    const hslColor = `hsl(${hslValue.h}, ${hslValue.s}%, ${l}%)`;
+    shades[key] = hslColor;
+  }
+
+  return shades;
+};
