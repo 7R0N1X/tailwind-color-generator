a3a584b40685262def6e137cbfcfa7a6bc74b0d8
7R0N1X <techsoftec.tronix@gmail.com>
2025-05-09 09:32:36 -0500

    feat: show current date

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,19 @@
 import { useColorPicker } from "@hooks/useColorPicker";
-import tinycolor from "tinycolor2";
 
 export const Card = () => {
   const { colorPalette } = useColorPicker();
-  const gradientColor = tinycolor(colorPalette["800"]).toHex();
-  const textColor = tinycolor(colorPalette["50"]).toHex();
+  const gradientColor = colorPalette["900"];
 
   return (
-    <div className="relative flex h-full w-full items-end overflow-hidden rounded-xl border border-gray-50/10 p-6">
-      <img src="/images/VzJjPuk53sk.avif" alt="Person writing code on a laptop" className="absolute inset-0 z-0 h-full w-full object-cover" />
-      <div className="z-10t absolute inset-0" style={{ backgroundImage: `linear-gradient(to top, #${gradientColor}, transparent)` }}></div>
-      <h2 className="relative z-20 text-4xl leading-[44px] font-semibold" style={{ color: `#${textColor}` }}>Modern Web Development</h2>
+    <div className="relative h-full w-full overflow-hidden rounded-xl border border-gray-50/10">
+      <img src="/images/VzJjPuk53sk.avif" alt="Person writing code on a laptop" className="h-full w-full object-cover"/>
+      <div className="z-10t absolute inset-0 -z-0" style={{ backgroundImage: `linear-gradient(to top, ${gradientColor}, transparent)` }}></div>
+      <div className="absolute inset-0 z-10 flex flex-col justify-end gap-2 p-6 text-gray-50">
+        <span>{new Date().toDateString()}</span>
+        <h2 className="relative z-20 text-4xl leading-[44px] font-semibold">
+          Modern Web Development
+        </h2>
+      </div>
     </div>
   );
 };
