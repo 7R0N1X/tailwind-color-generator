59d18c82a11716c6ffa2ad4aca4921324fcd3eb4
7R0N1X <techsoftec.tronix@gmail.com>
2025-05-09 10:36:37 -0500

    feat: add information about the publisher

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -1,16 +1,26 @@
 import { Badge } from "@components/Badge";
 import type { Article } from "@constants/articles";
+import { useColorPicker } from "@hooks/useColorPicker";
 
-export const ArticleCard = ({ imageUrl, alt, title, tags }: Article) => {
-  const color = "#FB2E38";
+export const ArticleCard = ({ imageUrl, alt, title, tags, avatarUrl, name }: Article) => {
+  const { colorPalette } = useColorPicker();
+  const bgColor = colorPalette["300"];
+  const textColor = colorPalette["50"];
   return (
-    <article className="flex gap-4 rounded-lg border-l-4 bg-neutral-800 p-3" style={{ borderColor: `${color}` }}>
+    <article className="flex gap-4 rounded-lg border-l-4 bg-neutral-800 p-3" style={{ borderColor: `${bgColor}` }}>
       <img src={imageUrl} alt={alt} className="size-20 rounded-xl object-cover" />
-      <div className="flex flex-col justify-center gap-2">
-        <h2 className="leading-[19px] font-medium text-gray-50">{title}</h2>
-        <div className="flex gap-2">
+      <div className="flex flex-col justify-between gap-2">
+        <h2 className="leading-5 font-medium" style={{ color: `${textColor}` }}>
+          {title}
+        </h2>
+        <div className="flex items-center justify-between gap-2">
+          <div className="flex items-center gap-2 text-gray-50">
+            <img src={avatarUrl} alt="" className="size-7 rounded-full" />
+            <span className="text-sm font-medium">{name}</span>
+          </div>
+
           {tags.map(({ tag }, index) => (
-            <Badge key={index} title={tag} bgColor="#FB2E38" />
+            <Badge key={index} title={tag} bgColor={bgColor} />
           ))}
         </div>
       </div>
--- src/components/ArticlesCard.tsx
+++ src/components/ArticlesCard.tsx
@@ -3,15 +3,9 @@ import { ARTICLES } from "@constants/articles";
 
 export const ArticlesCard = () => {
   return (
-    <div className="space-y-4 rounded-xl border border-gray-50/10 bg-neutral-900 p-4">
-      {ARTICLES.map(({ imageUrl, alt, title, tags }, index) => (
-        <ArticleCard
-          key={index}
-          imageUrl={imageUrl}
-          alt={alt}
-          title={title}
-          tags={tags}
-        />
+    <div className="space-y-4">
+      {ARTICLES.map(({ imageUrl, alt, title, tags, avatarUrl, name }, index) => (
+        <ArticleCard key={index} imageUrl={imageUrl} alt={alt} title={title} tags={tags} avatarUrl={avatarUrl} name={name} />
       ))}
     </div>
   );
