146ee5d2457abab1be2be04c0d8d77b236bc9c0f
7R0N1X <techsoftec.tronix@gmail.com>
2025-05-14 11:41:36 -0500

    refactor: enhance useTailwindColorForm for improved functionality and organization

--- src/hooks/useTailwindColorForm.ts
+++ src/hooks/useTailwindColorForm.ts
@@ -1,14 +1,21 @@
 import { generateColorPalette } from "@utils/generatePalette";
 import tinycolor from "tinycolor2";
-import type { ChangeEvent } from "react";
+import type { ChangeEvent, FormEvent } from "react";
+import { toast } from "react-toastify";
 
 interface useTailwindColorFormProps {
   setColorName: (name: string) => void;
+  colorHex: string;
   setColorHex: (hex: string) => void;
   setColorPalette: ({}) => void;
 }
 
-export const useTailwindColorForm = ({ setColorName, setColorHex, setColorPalette }: useTailwindColorFormProps) => {
+export const useTailwindColorForm = ({
+  setColorName,
+  setColorHex,
+  setColorPalette,
+  colorHex,
+}: useTailwindColorFormProps) => {
   const generatePalette = (colorHex: string) => {
     const hex = tinycolor(colorHex);
     const colorHsl = hex.toHslString();
@@ -32,5 +39,20 @@ export const useTailwindColorForm = ({ setColorName, setColorHex, setColorPalett
     setColorPalette(generatePalette(value));
   };
 
-  return { generatePalette, handleColorName, handleColorPicker, handleColorHex };
+  const generateColorName = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    fetch(`https://api.color.pizza/v1/?values=${colorHex.replace("#", "")}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const colorName = data.colors[0].name.toLowerCase().replace(/ /g, "-");
+        setColorName(colorName);
+        toast.success("Color name generated");
+      })
+      .catch((err) => {
+        console.error(err);
+        toast.error("Error generating color name");
+      });
+  };
+
+  return { generatePalette, handleColorName, handleColorPicker, handleColorHex, generateColorName };
 };
