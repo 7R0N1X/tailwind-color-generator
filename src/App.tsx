6a4d72623775bd6d0a5ee9fa3b5aac2417135200
7R0N1X <techsoftec.tronix@gmail.com>
2025-05-14 11:04:19 -0500

    refactor: reorder imports in App component for better organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { Header } from "@components/Header";
-import { ColorPalette } from "@components/ColorPalette";
-import { TailwindColorForm } from "@components/TailwindColorForm";
-import { UiExamples } from "@components/UiExamples";
 import { BlurredCircle } from "@components/BlurredCircle";
+import { ColorPalette } from "@components/ColorPalette";
 import { Footer } from "@components/Footer";
+import { Header } from "@components/Header";
+import { TailwindColorForm } from "@components/TailwindColorForm";
 import { ToastContainer } from "react-toastify";
+import { UiExamples } from "@components/UiExamples";
 export const App = () => {
   return (
     <div className="relative mx-auto max-w-[1489px] space-y-[60px] overflow-x-hidden pt-20 sm:pt-[96px]">
-      <ToastContainer />
+      <ToastContainer position="bottom-right" />
       <BlurredCircle />
       <Header />
       <TailwindColorForm />
