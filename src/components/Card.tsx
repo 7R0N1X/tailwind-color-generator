6904a6a9ee168de4aae6a51ad9ce0cd8d39f6f1b
7R0N1X <techsoftec.tronix@gmail.com>
2025-05-14 10:59:25 -0500

    refactor: update Card component for consistent styling and variable naming

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,17 +2,22 @@ import { useColorPicker } from "@hooks/useColorPicker";
 
 export const Card = () => {
   const { colorPalette } = useColorPicker();
-  const gradientColor = colorPalette["900"];
+  const variant900 = colorPalette["900"];
 
   return (
-    <div className="relative h-full w-full overflow-hidden rounded-xl border border-gray-50/10">
-      <img src="/images/VzJjPuk53sk.avif" alt="Person writing code on a laptop" className="h-full w-full object-cover"/>
-      <div className="z-10t absolute inset-0 -z-0" style={{ backgroundImage: `linear-gradient(to top, ${gradientColor}, transparent)` }}></div>
+    <div className="relative h-[436px] w-auto overflow-hidden rounded-xl border border-gray-50/10">
+      <img
+        src="/images/VzJjPuk53sk.avif"
+        alt="Person writing code on a laptop"
+        className="h-full w-full object-cover"
+      />
+      <div
+        className="z-10t absolute inset-0 -z-0"
+        style={{ backgroundImage: `linear-gradient(to top, ${variant900}, transparent)` }}
+      ></div>
       <div className="absolute inset-0 z-10 flex flex-col justify-end gap-2 p-6 text-gray-50">
         <span>{new Date().toDateString()}</span>
-        <h2 className="relative z-20 text-4xl leading-[44px] font-semibold">
-          Modern Web Development
-        </h2>
+        <h2 className="relative z-20 text-4xl leading-[44px] font-semibold">Modern Web Development</h2>
       </div>
     </div>
   );
