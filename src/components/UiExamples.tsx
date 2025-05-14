24147a375ba63e93a810646ca8abf0b28995a071
7R0N1X <techsoftec.tronix@gmail.com>
2025-05-14 11:03:52 -0500

    refactor: restructure UiExamples component for improved layout and organization

--- src/components/UiExamples.tsx
+++ src/components/UiExamples.tsx
@@ -1,13 +1,26 @@
+import { Banner } from "@components/Banner";
 import { Card } from "@components/Card";
-import { ArticlesCard } from "@components/ArticlesCard";
+import { CodeBlock } from "@components/CodeBlock";
+import { LoginForm } from "@components/LoginForm";
+import { PaymentCards } from "@components/PaymentCards";
 
 export const UiExamples = () => {
   return (
-    <section className="space-y-6 px-4 sm:px-6 2xl:px-0">
-      <h2 className="text-xl font-medium text-gray-50">UI Examples</h2>
-      <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        <Card />
-        <ArticlesCard />
+    <section className="px-4 text-gray-50 sm:px-6 2xl:px-0">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-4">
+        <div className="h-full space-y-4">
+          <h2 className="text-xl font-medium text-gray-50">Code</h2>
+          <CodeBlock />
+        </div>
+        <div className="space-y-4">
+          <h2 className="text-xl font-medium text-gray-50">UI Examples</h2>
+          <Card />
+        </div>
+        <div className="space-y-4 xl:mt-11">
+          <Banner />
+          <PaymentCards />
+        </div>
+        <LoginForm className="xl:mt-11" />
       </div>
     </section>
   );
