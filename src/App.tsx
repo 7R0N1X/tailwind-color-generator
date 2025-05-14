f700fc1296407923b8aa047e4604f6221f93cb36
7R0N1X <techsoftec.tronix@gmail.com>
2025-05-14 12:11:13 -0500

    feat: add BuyMeACoffee link and icon to support section

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { Header } from "@components/Header";
 import { TailwindColorForm } from "@components/TailwindColorForm";
 import { ToastContainer } from "react-toastify";
 import { UiExamples } from "@components/UiExamples";
+import { BuyMeACoffe } from "@assets/icons/BuyMeACoffee";
+
 export const App = () => {
   return (
     <div className="relative mx-auto max-w-[1489px] space-y-[60px] overflow-x-hidden">
@@ -14,6 +16,17 @@ export const App = () => {
       <TailwindColorForm />
       <ColorPalette />
       <UiExamples />
+
+      <a
+        href="https://buymeacoffee.com/tronix_dev"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Support this project at Buy Me a Coffee"
+        className="fixed right-4 bottom-0 z-50 inline-flex size-12 items-center justify-center rounded-full border border-gray-800/20 bg-gray-50 sm:right-6 sm:bottom-0 xl:right-8 xl:-bottom-7"
+      >
+        <BuyMeACoffe className="size-7" />
+      </a>
+
       <Footer />
     </div>
   );
